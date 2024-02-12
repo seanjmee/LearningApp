3ed7360e6e6d33f341e6583da0026abf5676e6ee
Sean Mee <sean.mee@hotmail.com>
2024-02-12 03:00:14 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Sean Mee on 2024-02-12.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
